dae0700acf13db7eda111078c706ded86845fca9
ivashkova <ivashkova@tutu.ru>
2018-10-15 18:10:07 +0300

    added error

--- index.php
+++ index.php
@@ -3,19 +3,27 @@ require_once('./init.php');
 
 if (!$connection) {
 	$error = mysqli_connect_error();
-	print($error);
+	$pageContent = includeTemplate('error_page.php', [
+		'error'	=> $error
+	]);
 } else {
 	$lotsSql = 'SELECT lot_id, name, started_price, image AS img_url, categories.title AS category_name FROM lots'
 		. ' JOIN categories ON lots.category_id = categories.category_id'
 		. ' WHERE date_closed IS NULL'
 		. ' ORDER BY date_created DESC';
-	$lotList = getDataFromDatabase($connection, $lotsSql); /* TODO error */
-}
+	$lotList = getDataFromDatabase($connection, $lotsSql);
 
-$pageContent = includeTemplate('index.php', [
-	'categoryList'	=> $categoryList,
-	'lotList'		=> $lotList,
-]);
+	if (!empty($lotList)) {
+		$pageContent = includeTemplate('index.php', [
+			'categoryList'	=> $categoryList,
+			'lotList'		=> $lotList,
+		]);
+	} else {
+		$pageContent = includeTemplate('error_page.php', [
+			'error'	=> $lotList
+		]);
+	}
+}
 
 $layoutContent = includeTemplate('layout.php', [
 	'title'			=> 'Главная',
